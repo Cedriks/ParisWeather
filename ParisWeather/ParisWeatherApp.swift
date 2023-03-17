19e26cfef75ab2f58bf33e8d617fd261826e6940
Cédrik SARAIVA <cedriksaraiva@gmail.com>
2023-03-17 13:28:20 +0100

    Initial Commit

---
+++ ParisWeather/ParisWeatherApp.swift
@@ -0,0 +1,20 @@
+//
+//  ParisWeatherApp.swift
+//  ParisWeather
+//
+//  Created by Cedrik on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ParisWeatherApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
